8cb908e672e9422dc1a896e07c5a910b36559993
Daniel <git@danielclaes.de>
2015-12-22 13:06:19 +0100

    halt buton fix

--- rpi-halt-btn.py
+++ rpi-halt-btn.py
@@ -7,15 +7,19 @@ HALT_SWITCH_GPIO_PIN = 25
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(HALT_SWITCH_GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+time.sleep(0.5)
 
 print "FinnCam: started and now waiting for GPIO halt button to be pressed."
 
 # wait for the button to be pressed and when pressed, issue
 # the halt command to stop the raspberry pi
-try:
-	GPIO.wait_for_edge(HALT_SWITCH_GPIO_PIN, GPIO.FALLING)
-except KeyboardInterrupt:
-	print "Stopped by user"
-	GPIO.cleanup()
-print "FinnCam: Shutdown switch pressed - halting system."
-subprocess.call(["halt"])
+while True:
+        try:
+                GPIO.wait_for_edge(HALT_SWITCH_GPIO_PIN, GPIO.FALLING)
+                print "FinnCam: Shutdown switch pressed - halting system."
+                subprocess.call(["halt"])
+        except KeyboardInterrupt:
+                print "Stopped by user"
+                GPIO.cleanup()
+        except:
+                continue
