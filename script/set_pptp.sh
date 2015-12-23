5d507ee64228a05a1e1f0671a75af092d9ae600c
Daniel <git@danielclaes.de>
2015-12-23 16:13:16 +0100

    upstart for pptp

--- script/set_pptp.sh
+++ script/set_pptp.sh
@@ -11,7 +11,11 @@ done
 
 IP=$(ifconfig wlan0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
 IP_PREF=$(echo $IP | grep -Eo '([0-9]{1,3}\.){3}')
+
+GWIP=$(/sbin/ip route | awk '/default/ { print $3 }')
+
 sed 's/localip \(.*\)/localip '"$IP"'/g' /etc/pptpd.conf -i
 sed 's/remoteip \(.*\)/remoteip '"$IP_PREF"'234-238/g' /etc/pptpd.conf -i
 
+sed 's/ms-dns 1\(.*\)/md-dns '"$GWIP"'/g' /etc/ppp/pptpd-options -i
 # upnpc -a $IP 1723 1723 TCP
