f05bfbbecfa70b7e0a4ae641ad55277bb1e00edd
Daniel <git@danielclaes.de>
2015-12-23 15:25:49 +0100

    upstart for pptp

--- script/set_pptp.sh
+++ script/set_pptp.sh
@@ -1,4 +1,17 @@
 #!/bin/env sh
+while true; do
+    # testing...
+    LC_ALL=C nmcli -t -f DEVICE,STATE dev | grep -q "^wlan0:connected$"
+    if [ $? -eq 0 ]; then
+	break
+    else
+	# not connected, sleeping for a second
+	sleep 1
+    fi
+done
+
+
+
 IP=$(ifconfig wlan0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
 IP_PREF=$(echo $IP | grep -Eo '([0-9]{1,3}\.){3}')
 sed 's/localip \(.*\)/localip '"$IP"'/g' /etc/pptpd.conf -i
